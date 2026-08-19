88bd3bb6f81aadef93d7dc394fa1fe50172050e9
mailphs2704-lgtm <mail.phs2704@gmail.com>
2026-08-19 13:04:10 +0700

    Handle Android system back navigation

--- app/src/main/java/com/cry/manage/MainActivity.kt
+++ app/src/main/java/com/cry/manage/MainActivity.kt
@@ -2,11 +2,15 @@ package com.cry.manage
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.activity.compose.setContent
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import com.cry.manage.feature.home.HomeScreen
 import com.cry.manage.feature.home.project.ProjectMenuScreen
 import com.cry.manage.ui.theme.CryManageTheme
-import androidx.compose.runtime.*
 
 class MainActivity : ComponentActivity() {
 
@@ -15,22 +19,27 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             CryManageTheme {
-
                 var isProjectMenu by remember {
                     mutableStateOf(false)
                 }
 
                 if (isProjectMenu) {
-
-                    ProjectMenuScreen()
-
+                    ProjectMenuScreen(
+                        onBackToHome = {
+                            isProjectMenu = false
+                        }
+                    )
                 } else {
-
                     HomeScreen(
                         onStartClick = {
                             isProjectMenu = true
                         }
                     )
+                }
+
+                BackHandler(enabled = isProjectMenu) {
+                    // ProjectMenuScreen tự xử lý các màn hình con.
+                    // Callback này chỉ là lớp an toàn ở cấp Activity.
                 }
             }
         }
